448ca1bd61e39eb0984508a877adbc7a142b2e43
Tân <nhattanphan2014@gmail.com>
2026-06-28 17:21:10 +0700

    Revert "."
    This reverts commit f1751bc19cd587a0bda9f9513c5331ae9ada45b2.

--- assets/js/index.js
+++
@@ -1,5 +0,0 @@
-import { portfolioData } from "./data.js";
-
-$("#profile_avatar").attr( "src", portfolioData.profile.avatar );
-$("#profile_name").text( portfolioData.profile.name );
-$("#profile_role").text( portfolioData.profile.role );
